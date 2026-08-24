1af52ff626f010f8f4eed21bb138fe75ab279805
schisavo <sebastianchisavoforero@gmail.com>
2026-08-23 19:15:45 -0500

    feat(movie-info): add cast carousel and crew carousel with clean images.

---
+++ src/data/cast.ts
@@ -0,0 +1,260 @@
+export interface CastMember {
+  actorName: string;
+  characterName: string;
+  actorImage: string;
+  characterImage: string;
+  team: string;
+  teamColor: string;
+}
+
+export const cast: CastMember[] = [
+  // ==========================================
+  // 🎭 BLOQUE 1: DR. DOOM / LEGAZGO STARK
+  // ==========================================
+
+  {
+    actorName: "Robert Downey Jr.",
+    characterName: "Doctor Doom",
+    actorImage: "/images/characters/actors/robert-downey-jr.webp",
+    characterImage: "/images/characters/villains/doctor-doom.webp",
+    team: "Villain",
+    teamColor: "#05a85c",
+  },
+
+  {
+    actorName: "Chris Evans",
+    characterName: "Steve Rogers",
+    actorImage: "/images/characters/actors/chris-evans.webp",
+    characterImage: "/images/characters/heroes/steve-rogers.webp",
+    team: "Unknown Role",
+    teamColor: "#737373",
+  },
+
+  {
+    actorName: "Tom Hiddleston",
+    characterName: "Loki",
+    actorImage: "/images/characters/actors/tom-hiddleston.webp",
+    characterImage: "/images/characters/heroes/loki.webp",
+    team: "Unknown Role",
+    teamColor: "#737373",
+  },
+
+  {
+    actorName: "Tobey Maguire",
+    characterName: "Spider-Man",
+    actorImage: "/images/characters/actors/tobey-maguire.webp",
+    characterImage: "/images/characters/heroes/spider-man-tobey.webp",
+    team: "Unknown Role",
+    teamColor: "#737373",
+  },
+
+  {
+    actorName: "Andrew Garfield",
+    characterName: "Spider-Man",
+    actorImage: "/images/characters/actors/andrew-garfield.webp",
+    characterImage: "/images/characters/heroes/spider-man-andrew.webp",
+    team: "Unknown Role",
+    teamColor: "#737373",
+  },
+
+  {
+    actorName: "Nicolas Cage",
+    characterName: "Ghost Rider",
+    actorImage: "/images/characters/actors/nicolas-cage.webp",
+    characterImage: "/images/characters/heroes/ghost-rider.webp",
+    team: "Unknown Role",
+    teamColor: "#737373",
+  },
+
+  // ==========================================
+  // 🦸‍♂️ BLOQUE 2: AVENGERS & MULTIVERSO
+  // ==========================================
+
+  {
+    actorName: "Chris Hemsworth",
+    characterName: "Thor",
+    actorImage: "/images/characters/actors/chris-hemsworth.webp",
+    characterImage: "/images/characters/heroes/thor.webp",
+    team: "Avengers",
+    teamColor: "#e23636",
+  },
+
+  {
+    actorName: "Anthony Mackie",
+    characterName: "Captain America (Sam Wilson)",
+    actorImage: "/images/characters/actors/anthony-mackie.webp",
+    characterImage: "/images/characters/heroes/sam-wilson-cap.webp",
+    team: "Avengers",
+    teamColor: "#e23636",
+  },
+
+  {
+    actorName: "Benedict Cumberbatch",
+    characterName: "Doctor Strange",
+    actorImage: "/images/characters/actors/benedict-cumberbatch.webp",
+    characterImage: "/images/characters/heroes/doctor-strange.webp",
+    team: "Avengers",
+    teamColor: "#e23636",
+  },
+
+  {
+    actorName: "Tom Holland",
+    characterName: "Spider-Man",
+    actorImage: "/images/characters/actors/tom-holland.webp",
+    characterImage: "/images/characters/heroes/spider-man-tom.webp",
+    team: "Avengers",
+    teamColor: "#e23636",
+  },
+
+  {
+    actorName: "Paul Rudd",
+    characterName: "Ant-Man",
+    actorImage: "/images/characters/actors/paul-rudd.webp",
+    characterImage: "/images/characters/heroes/ant-man.webp",
+    team: "Avengers",
+    teamColor: "#e23636",
+  },
+
+  {
+    actorName: "Sebastian Stan",
+    characterName: "Bucky Barnes / Winter Soldier",
+    actorImage: "/images/characters/actors/sebastian-stan.webp",
+    characterImage: "/images/characters/heroes/bucky-barnes.webp",
+    team: "Avengers",
+    teamColor: "#e23636",
+  },
+
+  {
+    actorName: "Florence Pugh",
+    characterName: "Yelena Belova",
+    actorImage: "/images/characters/actors/florence-pugh.webp",
+    characterImage: "/images/characters/heroes/yelena-belova.webp",
+    team: "Avengers",
+    teamColor: "#e23636",
+  },
+
+  {
+    actorName: "Simu Liu",
+    characterName: "Shang-Chi",
+    actorImage: "/images/characters/actors/simu-liu.webp",
+    characterImage: "/images/characters/heroes/shang-chi.webp",
+    team: "Avengers",
+    teamColor: "#e23636",
+  },
+
+  {
+    actorName: "Letitia Wright",
+    characterName: "Shuri / Black Panther",
+    actorImage: "/images/characters/actors/letitia-wright.webp",
+    characterImage: "/images/characters/heroes/shuri-black-panther.webp",
+    team: "Avengers",
+    teamColor: "#e23636",
+  },
+
+  // ==========================================
+  // ❌ BLOQUE 3: X-MEN & MUTANTES (LEGACY)
+  // ==========================================
+
+  {
+    actorName: "Patrick Stewart",
+    characterName: "Professor Charles Xavier",
+    actorImage: "/images/characters/actors/patrick-stewart.webp",
+    characterImage: "/images/characters/heroes/professor-x.webp",
+    team: "X-Men",
+    teamColor: "#F59E0B",
+  },
+
+  {
+    actorName: "Ian McKellen",
+    characterName: "Magneto",
+    actorImage: "/images/characters/actors/ian-mckellen.webp",
+    characterImage: "/images/characters/villains/magneto.webp",
+    team: "X-Men",
+    teamColor: "#F59E0B",
+  },
+
+  {
+    actorName: "Kelsey Grammer",
+    characterName: "Beast",
+    actorImage: "/images/characters/actors/kelsey-grammer.webp",
+    characterImage: "/images/characters/heroes/beast.webp",
+    team: "X-Men",
+    teamColor: "#F59E0B",
+  },
+
+  {
+    actorName: "James Marsden",
+    characterName: "Cyclops",
+    actorImage: "/images/characters/actors/james-marsden.webp",
+    characterImage: "/images/characters/heroes/cyclops.webp",
+    team: "X-Men",
+    teamColor: "#F59E0B",
+  },
+
+  {
+    actorName: "Channing Tatum",
+    characterName: "Gambit",
+    actorImage: "/images/characters/actors/channing-tatum.webp",
+    characterImage: "/images/characters/heroes/gambit.webp",
+    team: "X-Men",
+    teamColor: "#F59E0B",
+  },
+
+  {
+    actorName: "Alan Cumming",
+    characterName: "Nightcrawler",
+    actorImage: "/images/characters/actors/alan-cumming.webp",
+    characterImage: "/images/characters/heroes/nightcrawler.webp",
+    team: "X-Men",
+    teamColor: "#F59E0B",
+  },
+
+  {
+    actorName: "Rebecca Romijn",
+    characterName: "Mystique",
+    actorImage: "/images/characters/actors/rebecca-romijn.webp",
+    characterImage: "/images/characters/villains/mystique.webp",
+    team: "X-Men",
+    teamColor: "#F59E0B",
+  },
+
+  // ==========================================
+  // 🌌 BLOQUE 4: FANTASTIC FOUR
+  // ==========================================
+
+  {
+    actorName: "Pedro Pascal",
+    characterName: "Mister Fantastic (Reed Richards)",
+    actorImage: "/images/characters/actors/pedro-pascal.webp",
+    characterImage: "/images/characters/heroes/mr-fantastic.webp",
+    team: "Fantastic Four",
+    teamColor: "#1597d4",
+  },
+
+  {
+    actorName: "Vanessa Kirby",
+    characterName: "Invisible Woman (Sue Storm)",
+    actorImage: "/images/characters/actors/vanessa-kirby.webp",
+    characterImage: "/images/characters/heroes/invisible-woman.webp",
+    team: "Fantastic Four",
+    teamColor: "#1597d4",
+  },
+
+  {
+    actorName: "Joseph Quinn",
+    characterName: "Human Torch (Johnny Storm)",
+    actorImage: "/images/characters/actors/joseph-quinn.webp",
+    characterImage: "/images/characters/heroes/human-torch.webp",
+    team: "Fantastic Four",
+    teamColor: "#1597d4",
+  },
+
+  {
+    actorName: "Ebon Moss-Bachrach",
+    characterName: "The Thing (Ben Grimm)",
+    actorImage: "/images/characters/actors/ebon-moss-bachrach.webp",
+    characterImage: "/images/characters/heroes/the-thing.webp",
+    team: "Fantastic Four",
+    teamColor: "#1597d4",
+  },
+];
